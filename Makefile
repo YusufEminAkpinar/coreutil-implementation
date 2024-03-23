26478be60cf46c270767aa577735c5eaf003bd6b
YusufEminAkpinar <yakpinar137@gmail.com>
2024-03-23 09:26:50 +0300

    Finishing basic cat and starting to file command

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
-FLAGS=-Wall -Wextra -pedantic -ggdb
-LIBS=
-OUTDIR=./out/
+FLAGS := -Wall -Wextra -pedantic -ggdb
+LIBS :=
+# OUTDIR := ./out/
+
+all: file1 ls1 cat1
+
+file1: file1.c 
+	cc $(FLAGS) $(LIBS) -o $(OUTDIR)file1 file1.c
 
 ls1: ls1.c
 	cc $(FLAGS) $(LIBS) -o $(OUTDIR)ls1 ls1.c
+
+
+cat1: cat1.c
+	cc $(FLAGS) $(LIBS) -o $(OUTDIR)cat1 cat1.c
+
 clean:
 	test -n $(OUTDIR) && rm $(OUTDIR)*
+
+
--- cat1.c
+++ cat1.c
@@ -2,24 +2,39 @@
 #include "string.h"
 #include "stdlib.h"
 #include "limits.h"
+#include "sys/stat.h"
 
-
-void read_file_content(const char *path){
-  FILE *file = fopen(path, "r");
-	char *line = malloc(sizeof(char) * 5000);
-	if (file != NULL) {
-		while (fgets(line, INT_MAX, file) != NULL) {
-			printf("%s", line);
-		}
-	}
-  free(line);
-}
+void read_file_content(const char *path);
+// void read_from_stdin();
 
 
 int main(int argc, char *argv[])
 {
-  const char *path = "/home/hardal/test/file.js";
-  // FILE *file = fopen(path, "r");
-  read_file_content(path);
+  for (int i = 1; i < argc; i++) {
+    read_file_content(argv[i]);
+  }
   return 0;
 }
+
+// void read_from_stdin(){
+//   // char *buf = malloc(sizeof(char) * )
+//   // fgets
+// }
+
+void read_file_content(const char *path){
+  struct stat sb;
+  stat(path, &sb);
+  FILE *file = fopen(path, "r");
+  if (S_ISDIR(sb.st_mode)){
+    printf("%s: Is a directory.\n", path);
+    return;
+  }
+	if (file != NULL) {
+	  char *line = malloc(sizeof(char) * 5000);
+		while (fgets(line, INT_MAX, file) != NULL)
+			printf("%s", line);
+    free(line);
+	} else printf("%s: No such file.\n", path);
+}
+
+
